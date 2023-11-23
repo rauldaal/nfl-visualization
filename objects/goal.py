6b4b493b45ba8eb0b5193d3900ac2f27a86769d4
raul <rauldalgamonnialonso@gmail.com>
2023-11-23 13:09:32 +0100

    Fixed lint error

--- objects/goal.py
+++ objects/goal.py
@@ -1,14 +1,15 @@
 from .base import Objects
 
+
 class Porteria_Local(Objects):
     def __init__(self, app, vao_name='porteria_local', tex_id='porteria_local',
-                 pos=(0, 0, 0), rot=(-90, -90, 90), scale=(0.3,0.3,0.25)):
+                 pos=(0, 0, 0), rot=(-90, -90, 90), scale=(0.3, 0.3, 0.25)):
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
         self.on_init()
 
 
 class Porteria_Visitant(Porteria_Local):
     def __init__(self, app, vao_name='porteria_visitant', tex_id='porteria_visitant',
-                 pos=(0, 0, 0), rot=(180, 90, 180), scale=(0.3,0.3,0.25)):
+                 pos=(0, 0, 0), rot=(180, 90, 180), scale=(0.3, 0.3, 0.25)):
         super().__init__(app, vao_name, tex_id, pos, rot, scale)
         self.on_init()
